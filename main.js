82839ae99fbf74ae8fcba17067cbea0981420156
ItzArima <skaypaprilia@gmail.com>
2021-11-09 14:11:54 +0100

    bonus completato

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const app = new Vue({
     el: '#root',
     data:{
         emails : []
-    }
+    },
+    mounted() {
+        for(let i=0;i<10;i++){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(resp =>{
+                    var data = resp.data.response
+                    console.log(data);
+                    app.emails.push(data)
+                })
+        } 
+    }    
 })
 
-for(let i=0;i<10;i++){
-    axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(resp =>{
-            var data = resp.data.response
-            console.log(data);
-            app.emails.push(data)
-        })
-}        
+     
